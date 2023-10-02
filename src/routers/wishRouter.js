fd6d405ff5098902e891ee794b04ac37ff988d6f
MHRSRoni <mdmonirhosen.roni04@gmail.com>
2023-10-03 02:51:18 +0600

    wish router modified

--- src/routers/wishRouter.js
+++ src/routers/wishRouter.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const wishRouter = express.Router();
-const wishController = require('../controllers/wishController')
+const wishController = require('../controllers/wishController');
+const { isLogin, giveAccessTo } = require('../middlewares/auth');
 
 
 // wishList routes
 
-wishRouter.post('/wishlist-add/:foodId', wishController.addWishListController)
-wishRouter.get('/wish-by-id', wishController.getAllWishById)
-wishRouter.get('/wishlist-details', wishController.getWishListDetailsController)
-wishRouter.delete('/wishlist-remove/:foodId', wishController.removeWishListController)
+wishRouter.post('/wishlist-add/:foodId', isLogin, giveAccessTo('customer'), wishController.addWishListController)
+wishRouter.get('/wish-by-id', isLogin, giveAccessTo('customer'), wishController.getAllWishById)
+wishRouter.get('/wishlist-details', isLogin, giveAccessTo('customer'), wishController.getWishListDetailsController)
+wishRouter.delete('/wishlist-remove/:foodId', isLogin, giveAccessTo('customer'), wishController.removeWishListController)
 
 
 
 
-module.exports = wishRouter;
+module.exports = {wishRouter};
