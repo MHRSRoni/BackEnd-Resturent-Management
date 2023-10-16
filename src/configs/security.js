8c19f5f1430b61bb7ac135f825e10753b2a14a7f
Ali Rafat <coderrafat@gmail.com>
2023-10-16 12:33:39 +0600

    Update security.js

--- src/configs/security.js
+++ src/configs/security.js
@@ -26,7 +26,7 @@ exports.secure = (app) => {
     app.use(limiter)
 
     app.use(cors({
-        origin: 'http://127.0.0.1:5173',
+        origin: 'http://localhost:5173',
         credentials: true
     }))
 } 
